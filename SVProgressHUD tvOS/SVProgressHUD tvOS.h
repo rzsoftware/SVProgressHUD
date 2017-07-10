247e2382294ca17cc42afc647dbfe93e92a80fef
Ryan May <mayryana@gmail.com>
2017-07-10 11:01:53 -0400

    Added tvOS framework target

---
+++ SVProgressHUD tvOS/SVProgressHUD tvOS.h
@@ -0,0 +1,19 @@
+//
+//  SVProgressHUD tvOS.h
+//  SVProgressHUD tvOS
+//
+//  Created by Ryan May on 7/10/17.
+//  Copyright © 2017 EmbeddedSources. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVProgressHUD tvOS.
+FOUNDATION_EXPORT double SVProgressHUD_tvOSVersionNumber;
+
+//! Project version string for SVProgressHUD tvOS.
+FOUNDATION_EXPORT const unsigned char SVProgressHUD_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVProgressHUD_tvOS/PublicHeader.h>
+
+
